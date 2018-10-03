5eaffc5466d3d840c40c2635d3f134f35cbbdc7e
Miguel Orjuela <ma.orjuela73@gmail.com>
2018-10-03 09:49:38 -0500

    enabled cors, modified: student_serializer.rb

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,3 +1,4 @@
 class StudentSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :password_digest, :birth_date, :main_program
+  # attributes :id, :name, :email, :password_digest, :birth_date, :main_program
+  attributes :id, :name, :email, :birth_date, :main_program
 end
